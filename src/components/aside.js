ee03c7ea71235ba55cd0dc6e2ef9d56268c5f054
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-02-28 00:25:08 +0100

    feat: add GameNode logo and enhance aside component styling

--- src/components/aside.js
+++ src/components/aside.js
@@ -3,16 +3,14 @@ export function renderAside() {
 
   asideContainer.innerHTML = `
     <div class="mb-12">
-      <h1 class="text-xl font-title text-green-neon uppercase">
-        GameNode
-      </h1>
+      <img src="/src/assets/logo-gamenode.png" alt="GameNode Logo" class="w-24 h-24 mb-2 mx-auto">
+      <h1 class="text-2xl font-bold text-center text-(--green-neon) tracking-wide">GameNode</h1>
     </div>
 
     <nav class="flex-1 space-y-10">
       <section>
-        <p class="text-[10px] text-(--green-neon) uppercase tracking-widest mb-4 border-b border-card-color pb-1">Sistemas</p>
+        <p class="text-[10px] text-(--green-neon) uppercase tracking-widest mb-4 border-b border-card-color pb-1 shadow-[0_1px_2px_rgba(57,255,20,0.2)]">Sistemas</p>
           <ul id="aside-console-list" class="space-y-4 text-sm font-ui">
-              <li class="opacity-50">Cargando sistemas...</li>
           </ul>
       </section>
     </nav>
