80e95ceca45dc22cf132596a2abf668257548c69
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-02-28 00:47:30 +0100

    feat: update header navigation links to point to specific sections

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,9 @@ export function renderHeader() {
 
   headerContainer.innerHTML = `
     <nav class="flex gap-8 text-2xl font-ui uppercase">
-      <a href="#" class="text-(--green-neon) border-b border-green-neon pb-1 hover:opacity-80 transition-opacity">Inicio</a>
-      <a href="#" class="text-(--green-neon) hover:opacity-80 transition-opacity">Videojuegos</a>
-      <a href="#" class="text-(--green-neon) hover:opacity-80 transition-opacity">Consolas</a>
-      <a href="#" class="text-(--green-neon) hover:opacity-80 transition-opacity">Compañías</a>
+      <a href="#section-videogames" class="text-(--green-neon) hover:opacity-80 transition-opacity">Videojuegos</a>
+      <a href="#section-consoles" class="text-(--green-neon) hover:opacity-80 transition-opacity">Consolas</a>
+      <a href="#section-companies" class="text-(--green-neon) hover:opacity-80 transition-opacity">Compañías</a>
     </nav>
     
     <div class="flex items-center gap-4 w-full md:w-auto">
