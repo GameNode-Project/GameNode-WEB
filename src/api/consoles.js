e5cb09dae9b5dbaf79002860dde75b8555173fe7
7adrii <adriloma007@gmail.com>
2026-03-03 18:43:17 +0100

    feat: implement API call to load consoles and display loading state

--- src/api/consoles.js
+++ src/api/consoles.js
@@ -1,15 +1,38 @@
 import { createCard } from '../components/card.js';
-
-const mockConsoles = [
-  { id: 1, name: 'PlayStation 5', description: 'Potencia bruta con carga ultrarrápida SSD.', release_date: '2020-11-12', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/PlayStation_5_and_DualSense_with_transparent_background.png/1200px-PlayStation_5_and_DualSense_with_transparent_background.png', company_id: 2 }
-];
+import { showError } from '../utils/alerts.js';
 
 export async function loadConsoles() {
   const container = document.getElementById('grid-consoles');
+  
+// Mostramos estado de carga
+  container.innerHTML = `
+    <div class="col-span-full flex flex-col items-center justify-center py-12 gap-4">
+      <div class="w-12 h-12 border-4 border-[#3a3a3a] border-t-(--green-neon) rounded-full animate-spin drop-shadow-[0_0_10px_rgba(57,255,20,0.5)]"></div>
+      <p class="text-xs font-ui text-(--green-neon) animate-pulse tracking-widest uppercase opacity-80">Extrayendo datos...</p>
+    </div>
+  `;
+
   try {
-    const data = mockConsoles; // FUTURO FETCH AQUI
-    container.innerHTML = data.map(c => createCard(c, 'console')).join('');
+    const API_URL = import.meta.env.VITE_API_URL;
+    const response = await fetch(`${API_URL}/consoles`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const jsonResult = await response.json();
+
+    const consoles = jsonResult.data;
+
+    if(!consoles || consoles.length === 0) {
+      container.innerHTML = `<p class="text-sm font-ui text-gray-500">No hay consolas registradas todavia.</p>`;
+      return;
+    }
+
+    container.innerHTML = consoles.map(console => createCard(console, 'console')).join('');
+
   } catch (error) {
-    container.innerHTML = `<p class="text-red-500 font-ui text-sm">Error al cargar consolas.</p>`;
+    console.error("Error cargando consolas: ", error);
+    container.innerHTML = `<p class="text-red-500 font-ui text-sm">Error de conexión con el servidor.</p>`;
   }
 }
